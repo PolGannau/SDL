5808169855ad23998be5fa3f9c3af5f240087a93
Pol Gannau Coma <PolGannau@users.github.com>
2019-03-02 16:04:59 +0100

    Done first part

--- SDL/SDL_Project/Main.cpp
+++ SDL/SDL_Project/Main.cpp
@@ -1,8 +1,70 @@
-#include <stdio.h>
+#include <stdlib.h>
 
-int main()
+#include "../SDL/include/SDL.h"
+#pragma comment( lib, "../SDL/x86/SDL2.lib" )
+#pragma comment( lib, "../SDL/x86/SDL2main.lib" )
+
+int main(int argc, char* argv[])
 {
-	printf("Hello there");
+	SDL_Init(SDL_INIT_EVERYTHING);
 
+	SDL_Event event;
 
+	bool loop = true;
+
+	SDL_Window *win = SDL_CreateWindow("RedSquare",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		720,
+		720,
+		SDL_WINDOW_RESIZABLE);
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, 0);
+
+	SDL_Rect rect = {200, 200, 100, 100};
+
+	while (loop)
+	{
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderFillRect(renderer, &rect);
+		SDL_RenderPresent(renderer);
+
+		if (SDL_PollEvent(&event) != 0) {
+			switch (event.type)
+			{
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.sym) {
+                case SDLK_UP:
+                    if (rect.y > 0) {
+						rect.y -= 10;
+                    }
+                    break;
+                case SDLK_DOWN:
+                    if (rect.y < 720 - rect.h) {
+						rect.y += 10;
+                    }
+                    break;
+                case SDLK_RIGHT:
+                    if (rect.x < (720 - redsquare.w)) {
+						rect.x += 10;
+                    }
+                    break;
+                case SDLK_LEFT:
+                    if (rect.x > 0) {
+						rect.x -= 10;
+                    }
+                    break;
+                case SDLK_ESCAPE:
+                    loop = false;
+                    break;
+                }
+			}
+		}
+	}
+
+	SDL_Quit();
+	return 0;
 }
---
+++ SDL/SDL_Project/SDL.h
@@ -0,0 +1,7 @@
+#ifndef __SDL_H__
+#define __SDL_H__
+
+#pragma comment(lib, "../SDL/x86/SDL2.lib")
+#pragma comment(lib, "../SDL/x86/SDL2main.lib")
+
+#endif // !__SDL_H__
