be2fae585fb0609456d9295852019fd7b407aafa
Pol Gannau Coma <36154511+PolGannau@users.noreply.github.com>
2019-03-04 13:57:39 +0100

    Trying to shoot

--- SDL/SDL_Project/Main.cpp
+++ SDL/SDL_Project/Main.cpp
@@ -13,54 +13,72 @@ int main(int argc, char* argv[])
 	SDL_Window *win = SDL_CreateWindow("RedSquare",
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
-		720,
+		900,
 		720,
 		SDL_WINDOW_RESIZABLE);
 
 	SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, 0);
 
-	SDL_Rect rect = {200, 200, 100, 100};
+	SDL_Rect redrect = {50, 300, 100, 100};
+	SDL_Rect greenrect = { redrect.w/2, redrect.h/2, 50, 20 };
 
 	while (loop)
 	{
-		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-		SDL_RenderClear(renderer);
-
-		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-		SDL_RenderFillRect(renderer, &rect);
-		SDL_RenderPresent(renderer);
-
-		if (SDL_PollEvent(&event) != 0) {
+		// input
+		// logic
+		if (SDL_PollEvent(&event) != 0)
+		{
 			switch (event.type)
 			{
 			case SDL_KEYDOWN:
-				switch (event.key.keysym.sym) {
+				switch (event.key.keysym.sym) 
+				{
                 case SDLK_UP:
-                    if (rect.y > 0) {
-						rect.y -= 10;
-                    }
+					if (redrect.y > 0)
+					{
+						redrect.y -= 10;
+					}
                     break;
                 case SDLK_DOWN:
-                    if (rect.y < 720 - rect.h) {
-						rect.y += 10;
+                    if (redrect.y < 720 - redrect.h) 
+					{
+						redrect.y += 10;
                     }
                     break;
                 case SDLK_RIGHT:
-                    if (rect.x < (720 - rect.w)) {
-						rect.x += 10;
+                    if (redrect.x < (720 - redrect.w)) 
+					{
+						redrect.x += 10;
                     }
                     break;
                 case SDLK_LEFT:
-                    if (rect.x > 0) {
-						rect.x -= 10;
+                    if (redrect.x > 0) 
+					{
+						redrect.x -= 10;
                     }
                     break;
+				case SDLK_SPACE:
+					while (greenrect.x < (720 - greenrect.w))
+					{
+						greenrect.x += 15;
+					}
+					break;
                 case SDLK_ESCAPE:
                     loop = false;
                     break;
                 }
 			}
 		}
+		//print
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		SDL_RenderFillRect(renderer, &greenrect);
+
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderFillRect(renderer, &redrect);
+		SDL_RenderPresent(renderer);
 	}
 
 	SDL_Quit();
